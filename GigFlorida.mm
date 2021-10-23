e5fb750c873a30d2f7de563701c50c692cdf0a95
Kurt David Gooch <GigFlorida@aol.com>
2021-10-23 16:04:22 -0400

    Quick Git

---
+++ GigFlorida.mm
@@ -0,0 +1,161 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1634107985165" ID="ID_1878308159" MODIFIED="1634869721593" STYLE="bubble" TEXT="GigFlorida Network">
+<edge COLOR="#000000" WIDTH="8"/>
+<font NAME="Georgia" SIZE="20"/>
+<icon BUILTIN="idea"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1634108127629" ID="ID_651833811" MODIFIED="1634875801331" POSITION="right" TEXT="GigFlorida Network Publication">
+<edge COLOR="#0033ff" STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1634109256560" ID="ID_138321894" MODIFIED="1634875812216" TEXT="Scholarly Journal">
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1634865043018" ID="ID_1966109484" MODIFIED="1634868467449" TEXT="Professional Essays">
+<edge COLOR="#990000" WIDTH="1"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1634866211064" ID="ID_680181537" MODIFIED="1634868467449" TEXT="Peer Reveiwed Articles">
+<edge COLOR="#990000" WIDTH="1"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1634865845174" ID="ID_1380791684" MODIFIED="1634868467449" TEXT="Library of Music">
+<edge COLOR="#990000" WIDTH="1"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1634866093794" ID="ID_820429111" MODIFIED="1634868467450" TEXT="Music Education">
+<edge COLOR="#990000" WIDTH="1"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1634866149485" ID="ID_1438162716" MODIFIED="1634868467450" TEXT="Music History">
+<edge COLOR="#990000" WIDTH="1"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1634865987517" ID="ID_612375423" MODIFIED="1634868467450" TEXT="Travel to Florida">
+<edge COLOR="#990000" WIDTH="1"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1634109385109" ID="ID_1728086110" MODIFIED="1634875814017" TEXT="Trade Magazine">
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1634865090113" ID="ID_1727414933" MODIFIED="1634868467451" TEXT="Industry Collection">
+<edge COLOR="#990000" WIDTH="1"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1634865213646" ID="ID_303084539" MODIFIED="1634868467452" TEXT="Special Interest">
+<edge COLOR="#990000" WIDTH="1"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1634865797739" ID="ID_718732927" MODIFIED="1634868467452" TEXT="Gear Reveiws">
+<edge COLOR="#990000" WIDTH="1"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1634866009356" ID="ID_193445958" MODIFIED="1634868467452" TEXT="Travel to Florida">
+<edge COLOR="#990000" WIDTH="1"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1634109442756" ID="ID_1064221762" MODIFIED="1634875815503" TEXT="GigFlorida Gazetteer">
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1634865240693" ID="ID_102099555" MODIFIED="1634868467453" TEXT="Micro Niche Musician">
+<edge COLOR="#990000" WIDTH="1"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1634865284493" ID="ID_169778214" MODIFIED="1634868467454" TEXT="General Interest">
+<edge COLOR="#990000" WIDTH="1"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1634869590857" ID="ID_663460372" MODIFIED="1634869604522" TEXT="Classified Ads">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1634865725012" ID="ID_184511686" MODIFIED="1634868467454" TEXT="Sensational Magazine">
+<edge COLOR="#990000" WIDTH="1"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1634866032343" ID="ID_982678868" MODIFIED="1634868467454" TEXT="Travel to Florida">
+<edge COLOR="#990000" WIDTH="1"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1634864707179" ID="ID_293626447" MODIFIED="1634868467455" POSITION="left" TEXT="GigFlorida Social Network Home">
+<edge COLOR="#0033ff" STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1634865625415" FOLDED="true" ID="ID_552195850" MODIFIED="1634875792608" TEXT="GigFlorida Social Network, Drupal, Self Hosted">
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1634868742498" ID="ID_1461055501" MODIFIED="1634868803325" TEXT="GigFlorida Users">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1634868707790" ID="ID_974771125" MODIFIED="1634868812073" TEXT="GigFlorida Pages">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1634868860224" ID="ID_1640718886" MODIFIED="1634868883323" TEXT="GigFlorida Blogs">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1634868663612" ID="ID_906098719" MODIFIED="1634868820506" TEXT="GigFlorida Groups">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1634868686063" ID="ID_23848664" MODIFIED="1634868827523" TEXT="GigFlorida Forums">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1634866628416" ID="ID_1126485957" MODIFIED="1634868843112" TEXT="GigFlorida ...All Nodes Here!!!">
+<edge COLOR="#990000" WIDTH="1"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1634865390546" ID="ID_1482060807" MODIFIED="1634868467456" TEXT="SuperDesk NewsPlatform, Self Hosted">
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1634868933745" ID="ID_1921726459" MODIFIED="1634868945737" TEXT="AP RSS">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1634868951501" ID="ID_1222974194" MODIFIED="1634868958898" TEXT="NPR RSS">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1634868965174" ID="ID_341781000" MODIFIED="1634868978420" TEXT="Local News RSS">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1634869005199" ID="ID_1520028410" MODIFIED="1634869025343" TEXT="GigFlorida Entertainment News">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1634869125329" ID="ID_730855135" MODIFIED="1634869273420" TEXT="Florida State News RSS">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1634869099494" ID="ID_1848454726" MODIFIED="1634869264994" TEXT="SouthEast Regional News RSS">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1634869079593" ID="ID_31739503" MODIFIED="1634869257533" TEXT="United States National News RSS">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1634869053821" ID="ID_554026499" MODIFIED="1634869249600" TEXT="International World News RSS">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1634865484026" ID="ID_1269304667" MODIFIED="1634868467456" TEXT="Rocket Chat with PopOut, Self Hosted">
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1634866507715" ID="ID_245735958" MODIFIED="1634869466936" TEXT="GigFlorida Cloud Network, Self Hosted">
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1634869471381" ID="ID_132103183" MODIFIED="1634869482609" TEXT="Office">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1634869488215" ID="ID_796615262" MODIFIED="1634869491391" TEXT="Studio">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1634868518004" ID="ID_1265607312" MODIFIED="1634869399756" TEXT="Email Server for Marketing, Self Hosted">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1634866367693" ID="ID_1784690479" MODIFIED="1634868467457" TEXT="Travel to Florida">
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
